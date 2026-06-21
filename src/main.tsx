422b6c6ec04d9e4c4f7e77bf1e86c709bccb6833
nagix123-e <nagix@nifty.com>
2026-06-21 13:54:06 +0900

    add font for win

--- src/main.tsx
+++ src/main.tsx
@@ -1,5 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import "@fontsource/league-spartan/latin-900.css";
 import "katex/dist/katex.min.css";
 import "./styles.css";
 import App from "./App";
